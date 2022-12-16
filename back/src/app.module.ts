f731f4d2a2969164ec42379797bd11909572fcea
안형노 <hrahn@estsecurity.com>
2022-12-16 11:19:21 +0900

    typeorm 연결설정

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      // entities: [ './**/*.entity{.ts,.js}'],
+      // migrations: ['./database/migrations/**/*{.ts,.js}'],
+      synchronize: false,
+      logging: true,
+      keepConnectionAlive: true, // TODO: deprecated 됨
+      charset: 'utf8mb4',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
